fca1ada34bd133193b89e9bacc205f27fcbe8ba4
yashpokar <yash.p@digitalshelfiq.com>
2019-01-20 00:21:26 +0530

    frontend forms ready with validation

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,50 @@
 import React, { Component } from 'react';
 
+import axios from 'axios';
+
 import './App.sass';
 
 export default class App extends Component {
+	state = { address: '', error: '' }
+
 	findOutletNearAddress (e) {
+		const { address } = this.state;
+
 		e.preventDefault();
+
+		if (! address) {
+			return this.setState({ error: 'Please set the address first.' });
+		}
+
+		axios.get('http://127.0.0.1:5000', { params: { address } })
+			.then(({data}) => {
+				console.log(data);
+			})
+			.catch(err => this.setState({ error: err.message }));
 	}
 
 	render () {
+		const { address, error } = this.state;
+
 		return (
-			<div className="flex middle center fullwidth fullheight">
+			<div className="flex middle center as-column fullwidth fullheight">
+				<div className={ `error${ error ? ' show' : '' }` }>{ error }</div>
+
 				<div className="card">
 					<h2 className="card__title">Find Outlet Near Your Address</h2>
 
 					<div className="card__body">
 						<form onSubmit={ this.findOutletNearAddress.bind(this) }>
-							<input type="text" className="input-box fullwidth" placeholder="Enter your address here" />
+							<input
+								type="text"
+								className="input-box fullwidth"
+								placeholder="Enter your address here"
+								onChange={ e => this.setState({ address: e.target.value, error: '' }) }
+								value={ address } />
 
-							<button className="button fullwidth">Find Outlet</button>
+							<button className="button fullwidth">
+								Find Outlet
+							</button>
 						</form>
 					</div>
 				</div>
